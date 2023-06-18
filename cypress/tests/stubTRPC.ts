a858f0cc265fa6d79c8f1d0d69caad010c3e2565
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-06-18 11:04:56 -0300

    Test: Add superjson transfomer test

--- cypress/tests/stubTRPC.ts
+++ cypress/tests/stubTRPC.ts
@@ -1,17 +1,19 @@
 import { createTRPCProxyClient, httpLink } from '@trpc/client'
 import { initTRPC } from '@trpc/server'
+import superjson from 'superjson'
 import { stubTRPC } from '../../src'
 
 describe('stubTRPC', () => {
-  it('should work', async () => {
-    const t = initTRPC.create({ allowOutsideOfServer: true })
-    const router = t.router({
+  const t = initTRPC.create({ allowOutsideOfServer: true })
+  const router = t.router({
+    hello: t.procedure.query(() => 'world'),
+    nested: t.router({
       hello: t.procedure.query(() => 'world'),
-      nested: t.router({
-        hello: t.procedure.query(() => 'world'),
-      }),
-    })
-    type Router = typeof router
+    }),
+  })
+  type Router = typeof router
+
+  it('should handle no options passed', async () => {
     const trpcStub = stubTRPC<Router>()
 
     const client = createTRPCProxyClient<Router>({
@@ -22,5 +24,23 @@ describe('stubTRPC', () => {
 
     const result = await client.hello.query()
     expect(result).equal('interceptor')
+  })
+
+  it('should handle superjson transformer', async () => {
+    const trpcStub = stubTRPC<Router>({
+      transformer: {
+        input: superjson,
+        output: superjson,
+      },
+    })
+
+    const client = createTRPCProxyClient<Router>({
+      links: [httpLink({ url: '/api/trpc' })],
+    })
+
+    trpcStub.hello.returns('interceptor')
+
+    const result = await client.hello.query()
+    expect(result).deep.equal({ json: 'interceptor' })
   })
 })
